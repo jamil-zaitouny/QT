4f79f94a46a5d7f6e27f8ebc3addbf62399d556f
Jamil Zaitouny <jamil.zaitouny@gmail.com>
2019-05-27 15:31:03 +0300

    Adds the icons to the resouce file

--- Projects/Notepad/notepad.h
+++ Projects/Notepad/notepad.h
@@ -2,9 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+
 namespace Ui {
 class MainWindow;
 }
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -12,8 +21,43 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    // When the new Icon is pressed the textEdit be cleared
+    void on_actionNew_triggered();
+
+    //A prompt will ask the user to choose a file for reading
+    void on_actionOpen_triggered();
+
+    //The user will be prompted to save the file with a QFilePrompt
+    void on_actionSave_triggered();
+
+    //The user will prompted to choose the printer that he wants to print
+    //the content of the textEdit
+    void on_actionPrint_triggered();
+
+    //The window will be closed
+    void on_actionExit_triggered();
+
+    //All the text inside the textEdit will be copited to the clipboard
+    void on_actionCopy_triggered();
+
+    //Everything in the clipboard will be pasted to the current textEdit
+    void on_actionPaste_triggered();
+
+    //All the text inside the textEdit will be cut to the clipboard
+    void on_actionCut_triggered();
+
+    //The last text edit will be undone
+    void on_actionUndo_triggered();
+
+    //The last undo will redone
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
+    //Name of the currentFile if there's one
+    QString currentFile = "";
 };
 
 #endif // MAINWINDOW_H
